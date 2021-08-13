f889b99217edc64f6a78391b0414107af2ef4f6d
Showbiztable <40076377+Showbiztable@users.noreply.github.com>
2021-08-13 15:59:54 -0300

    Create NotFound.jsx
    Tela padrão do app para o erro 404.

---
+++ src/views/examples/NotFound.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+const NotFound = (props) => {
+    return (
+        <div className="NotFound">
+            <h1>Erro 404</h1>
+            <p>Essa página não existe</p>
+        </div>
+    )
+}
+export default NotFound
