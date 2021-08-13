65cd12845595bded4235199284504cf69bdf1a38
Showbiztable <40076377+Showbiztable@users.noreply.github.com>
2021-08-13 16:00:40 -0300

    Create About.jsx
    Tela das informações sobre o app.

---
+++ src/views/examples/About.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+class About extends React.Component {
+    render() {
+        return (
+            <div className="About">
+                <h1>Sobre</h1>
+                <p>O nosso sistema foi criado...</p>
+            </div>
+        )
+    } 
+}
+
+export default About
