418437db913faf6843acbc2c781954644a6833ce
Showbiztable <40076377+Showbiztable@users.noreply.github.com>
2021-08-13 16:00:16 -0300

    Create Home.jsx
    Página home do app.

---
+++ src/views/examples/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+class Home extends React.Component {
+    render() {
+        return (
+            <div className="Home">
+                <h1>Tela Inicial</h1>
+                <p>Seja bem vindo usuário...</p>
+            </div>
+        )
+    } 
+}
+
+export default Home
